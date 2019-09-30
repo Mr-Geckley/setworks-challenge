80b7d07d84f658773c78b30ff167827f6bdc35eb
Geckley <techncially.geckley@gmail.com>
2019-09-29 22:16:09 -0500

    add button to remove dynamically generated symptoms list items

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
+let numberOfSideFx = 0;
+
 function addSideFx() {
-  //   console.log("something happened I guess");
+  // target the <ul>
   var sideFxList = document.getElementById("side-fx-list");
+  // create an <li>
   var sideFxListItem = document.createElement("LI");
+  // create a var that holds a string equal to what ever is in the text input
   var sideFxListItemContent = document.getElementById("side-fx-list-input")
     .value;
-  sideFxListItem.innerHTML = sideFxListItemContent;
+  // set the content of said <li> to equal what is in the text input field
+  sideFxListItem.innerHTML = ` ${sideFxListItemContent} <button id="removeButton${numberOfSideFx}" type="button" onClick="removeSideFx(this.id)"> REMOVE </button>`;
 
+  // add the populated <li> to the <ul>
   sideFxList.appendChild(sideFxListItem);
+
+  numberOfSideFx++;
+}
+
+function wutevs() {
+  console.log("wutevs");
+}
+
+function removeSideFx(n) {
+  var targetElement = document.getElementById(n);
+  targetElement.parentNode.remove();
 }
